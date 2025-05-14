d158d6c4a50b4d1e62d8076a2d08c0239d8d8c4b
Kauanny19 <kauannycristina01n@gmail.com>
2025-05-14 15:12:30 -0300

    Limpando comentários do código

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,65 +1,30 @@
 const router = require("express").Router();
-
 const userController = require("../controller/userController");
 const classroomController = require("../controller/classroomController");
 const controllerReserva = require("../controller/controllerReserva");
-
 const verifyJWT = require("../services/verifyJWT");
 
 //User
 router.post("/user/", userController.createUser);
-// http://10.89.240.73:3000/api/user/
-
 router.post("/user/login", userController.postLogin);
-// http://10.89.240.73:3000/api/user/login
-
 router.get("/user/", userController.getAllUsers);
-// http://10.89.240.73:3000/api/user/
-
 router.get("/user/:id", userController.getUserById);
-// http://10.89.240.73:3000/api/user/id
-
-// JWT User na delete e Update 
 router.put("/user", verifyJWT, userController.updateUser); 
-// http://10.89.240.73:3000/api/user/
-
 router.delete("/user/:id", verifyJWT, userController.deleteUser); 
-// http://10.89.240.73:3000/api/user/id/
-
 
 //Classroom
 router.post("/sala/", classroomController.createClassroom);
-// http://10.89.240.73:3000/api/sala
-
 router.get("/sala/", classroomController.getAllClassrooms);
-// http://10.89.240.73:3000/api/sala
-
 router.get("/sala/:numero", classroomController.getClassroomById);
-// http://10.89.240.73:3000/api/sala/numero
-
 router.put("/sala/", classroomController.updateClassroom);
-// http://10.89.240.73:3000/api/sala
-
 router.delete("/sala/:numero", classroomController.deleteClassroom);
-// http://10.89.240.73:3000/api/sala/numero
-
 
 // Reservas
 router.post("/reserva/", controllerReserva.createReserva);
-// http://10.89.240.73:3000/api/reserva
-
 router.get("/reserva/", controllerReserva.getReservas);
-// http://10.89.240.73:3000/api/reserva
-
 router.put("/reserva/:id", controllerReserva.updateReserva);
-// http://10.89.240.73:3000/api/reserva/id
-
 router.delete("/reserva/:id_reserva", controllerReserva.deleteReserva);
-// http://10.89.240.73:3000/api/reserva/id_reserva
-
 router.get("/reserva/horarios/:id_sala/:data", controllerReserva.getHorariosSala);
-// http://10.89.240.73:3000/api/reserva/horarios/:id_sala/:data
-
 
 module.exports = router;
 
--- src/services/verifyJWT.js
+++ src/services/verifyJWT.js
@@ -1,36 +1,28 @@
-// Importa o módulo 'jsonwebtoken', usado para criar e verificar tokens JWT (JSON Web Token)
 const jwt = require("jsonwebtoken");
 
 // Define a função middleware 'verifyJWT' para verificar a autenticidade do token JWT
 function verifyJWT(req, res, next) {
-    // Obtém o token JWT do cabeçalho HTTP "authorization"
     const token = req.headers["authorization"];
 
-    // Se o token não for fornecido, retorna erro 401 (Unauthorized)
     if (!token) {
         return res.status(401).json({
-            auth: false, // Indica que o usuário não está autenticado
-            message: "Token não foi fornecido" // Mensagem explicando o erro
+            auth: false,
+            message: "Token não foi fornecido" 
         });
     }
 
     // Verifica se o token é válido usando a chave secreta armazenada na variável de ambiente SECRET
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        // Se ocorrer um erro na verificação (token inválido, expirado, etc.)
         if (err) {
             return res.status(403).json({
-                auth: false, // Indica que o usuário não está autenticado
-                message: "Falha na autenticação do Token" // Mensagem explicando o erro
+                auth: false, 
+                message: "Falha na autenticação do Token"
             });
         }
-
         // Se o token for válido, salva o ID do usuário extraído do token na requisição
         req.userId = decoded.id;
-
-        // Chama o próximo middleware ou rota, já que o token foi verificado com sucesso
         next();
     });
 }
 
-// Exporta a função para que ela possa ser usada em outras partes da aplicação
 module.exports = verifyJWT;
